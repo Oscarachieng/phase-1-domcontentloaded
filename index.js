1de3909fbdee5f1c2d3f6ded1221acb9562d82d3
Oscar Abuto Achieng <oscarachieng94@gmail.com>
2022-06-13 14:42:21 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function (e) {
+    let myParagraph = document.getElementById("text");
+    myParagraph.textContent = "This is really cool!";
+})
